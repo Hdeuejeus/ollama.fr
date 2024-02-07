1e6c225f3e8749ef109edd4009d1b5f02c69cbdd
Hdeuejeus <157997568+Hdeuejeus@users.noreply.github.com>
2024-02-07 10:34:54 +0100

    ollama

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -4,120 +4,120 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 export default [
   {
     path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug', '612'),
+    component: ComponentCreator('/__docusaurus/debug', 'c2a'),
     exact: true
   },
   {
     path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config', '782'),
+    component: ComponentCreator('/__docusaurus/debug/config', 'eb7'),
     exact: true
   },
   {
     path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content', '3ca'),
+    component: ComponentCreator('/__docusaurus/debug/content', 'efc'),
     exact: true
   },
   {
     path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData', '6c2'),
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c0f'),
     exact: true
   },
   {
     path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata', '101'),
+    component: ComponentCreator('/__docusaurus/debug/metadata', 'cf5'),
     exact: true
   },
   {
     path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry', 'b0a'),
+    component: ComponentCreator('/__docusaurus/debug/registry', '6e4'),
     exact: true
   },
   {
     path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes', '727'),
+    component: ComponentCreator('/__docusaurus/debug/routes', '772'),
     exact: true
   },
   {
     path: '/blog',
-    component: ComponentCreator('/blog', '114'),
+    component: ComponentCreator('/blog', '7ee'),
     exact: true
   },
   {
     path: '/blog/archive',
-    component: ComponentCreator('/blog/archive', 'bbf'),
+    component: ComponentCreator('/blog/archive', '540'),
     exact: true
   },
   {
     path: '/blog/first-blog-post',
-    component: ComponentCreator('/blog/first-blog-post', '7cf'),
+    component: ComponentCreator('/blog/first-blog-post', '745'),
     exact: true
   },
   {
     path: '/blog/long-blog-post',
-    component: ComponentCreator('/blog/long-blog-post', '984'),
+    component: ComponentCreator('/blog/long-blog-post', '9f0'),
     exact: true
   },
   {
     path: '/blog/mdx-blog-post',
-    component: ComponentCreator('/blog/mdx-blog-post', 'd43'),
+    component: ComponentCreator('/blog/mdx-blog-post', '6cf'),
     exact: true
   },
   {
     path: '/blog/tags',
-    component: ComponentCreator('/blog/tags', 'e55'),
+    component: ComponentCreator('/blog/tags', '09f'),
     exact: true
   },
   {
     path: '/blog/tags/docusaurus',
-    component: ComponentCreator('/blog/tags/docusaurus', '734'),
+    component: ComponentCreator('/blog/tags/docusaurus', '35d'),
     exact: true
   },
   {
     path: '/blog/tags/facebook',
-    component: ComponentCreator('/blog/tags/facebook', 'cf3'),
+    component: ComponentCreator('/blog/tags/facebook', 'ea6'),
     exact: true
   },
   {
     path: '/blog/tags/hello',
-    component: ComponentCreator('/blog/tags/hello', '33f'),
+    component: ComponentCreator('/blog/tags/hello', '0cb'),
     exact: true
   },
   {
     path: '/blog/tags/hola',
-    component: ComponentCreator('/blog/tags/hola', '1a8'),
+    component: ComponentCreator('/blog/tags/hola', 'ec4'),
     exact: true
   },
   {
     path: '/blog/welcome',
-    component: ComponentCreator('/blog/welcome', 'd5d'),
+    component: ComponentCreator('/blog/welcome', 'e18'),
     exact: true
   },
   {
     path: '/markdown-page',
-    component: ComponentCreator('/markdown-page', '0cb'),
+    component: ComponentCreator('/markdown-page', '619'),
     exact: true
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', '0fb'),
+    component: ComponentCreator('/docs', '611'),
     routes: [
       {
         path: '/docs',
-        component: ComponentCreator('/docs', '6e1'),
+        component: ComponentCreator('/docs', 'ece'),
         routes: [
           {
             path: '/docs',
-            component: ComponentCreator('/docs', 'd70'),
+            component: ComponentCreator('/docs', '427'),
             routes: [
               {
-                path: '/docs/category/en-savoir-plus',
-                component: ComponentCreator('/docs/category/en-savoir-plus', 'f9d'),
+                path: '/docs/category/bibliothèque',
+                component: ComponentCreator('/docs/category/bibliothèque', '724'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/category/llm---bibliothèque',
-                component: ComponentCreator('/docs/category/llm---bibliothèque', 'c32'),
+                path: '/docs/category/en-savoir-plus',
+                component: ComponentCreator('/docs/category/en-savoir-plus', 'f9d'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
@@ -140,44 +140,50 @@ export default [
                 sidebar: "tutorialSidebar"
               },
               {
+                path: '/docs/En-savoir-plus/instruct',
+                component: ComponentCreator('/docs/En-savoir-plus/instruct', 'd1e'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/En-savoir-plus/Quantization',
+                component: ComponentCreator('/docs/En-savoir-plus/Quantization', '44f'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
                 path: '/docs/intro',
                 component: ComponentCreator('/docs/intro', 'aed'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-basics/congratulations',
-                component: ComponentCreator('/docs/tutorial-basics/congratulations', '793'),
+                path: '/docs/models-list/Llama2',
+                component: ComponentCreator('/docs/models-list/Llama2', '7a2'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-basics/create-a-blog-post',
-                component: ComponentCreator('/docs/tutorial-basics/create-a-blog-post', '68e'),
+                path: '/docs/models-list/llama2-uncensored',
+                component: ComponentCreator('/docs/models-list/llama2-uncensored', '1b8'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-basics/create-a-document',
-                component: ComponentCreator('/docs/tutorial-basics/create-a-document', 'c2d'),
+                path: '/docs/models-list/Llava',
+                component: ComponentCreator('/docs/models-list/Llava', 'c00'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-basics/create-a-page',
-                component: ComponentCreator('/docs/tutorial-basics/create-a-page', 'f44'),
+                path: '/docs/models-list/Mixtral',
+                component: ComponentCreator('/docs/models-list/Mixtral', '26a'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/docs/tutorial-basics/deploy-your-site',
-                component: ComponentCreator('/docs/tutorial-basics/deploy-your-site', 'e46'),
-                exact: true,
-                sidebar: "tutorialSidebar"
-              },
-              {
-                path: '/docs/tutorial-basics/markdown-features',
-                component: ComponentCreator('/docs/tutorial-basics/markdown-features', '4b7'),
+                path: '/docs/models-list/TinyLlama',
+                component: ComponentCreator('/docs/models-list/TinyLlama', 'cde'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
@@ -201,7 +207,7 @@ export default [
   },
   {
     path: '/',
-    component: ComponentCreator('/', 'f1b'),
+    component: ComponentCreator('/', '351'),
     exact: true
   },
   {
---
+++ docs/models-list/Llama2.md
@@ -0,0 +1,18 @@
+---
+sidebar_position: 1
+---
+
+# Llama2 par Meta
+
+*le LLM Llama2 est un modèle de langage naturel développé par des 
+chercheurs à la recherche en intelligence artificielle (AI) de Facebook. Voici quelques points sur ce modèle:*
+
+1. ***Tailles de modèles variées** : LLaMA 2 est disponible en trois tailles : 7 milliards, 13 milliards et 70 milliards de paramètres, offrant une flexibilité en fonction des ressources de calcul et des besoins en précision.*
+
+2. ***Méthode d’entraînement innovante** : LLaMA 2 est formé à l’aide de l’apprentissage par renforcement à partir des commentaires humains (RLHF), en apprenant des préférences et des évaluations des formateurs d’IA humains.*
+
+3. ***Améliorations par rapport à son prédécesseur** : Le modèle LLaMA 2 a été pré-formé sur 40 % de données supplémentaires par rapport à son prédécesseur, avec plus d’un million d’annotations par des humains.*
+
+4. ***Partenariat avec Microsoft** : Meta s’est associé à Microsoft pour fournir une infrastructure informatique clé et un support via Azure et Windows, permettant aux développeurs du monde entier de tirer parti du nouveau LLM.*
+
+*Pour obtenir plus d'informations rendez-vous sur le [site](https://llama.meta.com/) de llama ou sur le [projet github](https://github.com/facebookresearch/llama)*
